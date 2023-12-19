df2f0a81c816d60c89a3a264f30cb548064be82f
aldhi saputra <aldhisaputra795@gmail.com>
2023-12-19 10:20:24 +0700

    Menambahkan API Update photo profile & Route yang API yang telah dibuat

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,6 +10,7 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -114,6 +115,36 @@ class UserController extends Controller
         $user->update($data);
 
         return ResponseFormatter::success($user, 'Profile Update');
+    }
+
+    //API upload photo
+    public function updatePhoto(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'file' =>'required|image|max:2048'
+        ]);
+
+        if($validator->fails())
+        {
+            return ResponseFormatter::error(
+                ['error' => $validator->errors()],
+                'Update photo fails',
+                401
+            );
+        }
+
+        if($request->file('file'))
+        {
+            $file = $request->file->store('assets/user','public');
+
+            //Simpan foto ke database (urlnya)
+            $user = Auth::user();
+            $user->profile_photo_path = $file;
+            $user->update();
+
+            return ResponseFormatter::success([$file], 'File successfully upload');
+
+        }
     }
 
     
