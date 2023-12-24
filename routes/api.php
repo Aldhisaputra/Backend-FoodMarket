569974b4de594e065c2e7c7d68f588f9c45c8045
aldhi saputra <aldhisaputra795@gmail.com>
2023-12-24 22:36:08 +0700

    clear chache route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\API\FoodController;
 use App\Http\Controllers\API\MidtransController;
 use App\Http\Controllers\API\TransactionController;
 use App\Http\Controllers\API\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
