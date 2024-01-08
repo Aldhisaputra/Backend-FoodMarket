cbbd75b8673837aa23c23b54a15b529faaa4eadd
aldhi saputra <aldhisaputra795@gmail.com>
2024-01-08 19:52:52 +0700

    Menambahkan DashboardController dan memnghapus kodingan di route web.php

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
 });
 
-Route::get('/debug-sentry', function (){
-    throw new Exception('My First Sentry error!');
-});
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
